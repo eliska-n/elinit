62786a49acd205243fc2f93578c9e2da4c154a44
eliska <eliska.novotna@protonmail.com>
2021-09-11 14:10:08 +0200

    schovala jsem secretkey do .env

--- elinit/settings.py
+++ elinit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#k@)q1j$ge3eojvy_h#*qbteo^290rnvz@853r4=_51fx-eko!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

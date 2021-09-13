7cf2aa2b1b609edf0606f0a8b0428ee02874fca6
eliska <eliska.novotna@protonmail.com>
2021-09-13 22:36:47 +0200

    debug false allowed_hosts elinit

--- elinit/settings.py
+++ elinit/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.elinit.cz']
 
 
 # Application definition

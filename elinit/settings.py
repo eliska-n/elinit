71a0ff879cefefd99b1d36800bc5ba1e4b129974
eliska <eliska.novotna@protonmail.com>
2021-09-13 22:53:44 +0200

    zpet na debug true a push na heroku

--- elinit/settings.py
+++ elinit/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.elinit.cz']
+ALLOWED_HOSTS = []
 
 
 # Application definition

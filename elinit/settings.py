74c0f60d0573080ff3a0b7755c25461adac0fe62
eliska <eliska.novotna@protonmail.com>
2021-09-22 17:22:49 +0200

    debug true aby to bezelo - potrebuju do cv

--- elinit/settings.py
+++ elinit/settings.py
@@ -27,10 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
